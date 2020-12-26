e2815dc0b33d6dcf1977013c7ce23e05d5772dac
INFy93 <infy93@gmail.com>
2020-12-26 09:17:23 +0300

    Fix 'Helpers' class

--- app/Helpers/core.php
+++ app/Helpers/core.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace App\Helpers;
+
 use App\Models\Epg;
 use App\Models\Itv;
 use App\Models\Logos;
 
-class ProgrammList {
-        public static function getBeforeProgramm($id) {
-            $time = date("Y-m-d H:i:s");
-            $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
+class ProgrammList
+{ //формируем телепрограмму
+    public static function getBeforeProgramm($id)
+    { //получаем передачи, время которых еще не пришло
+        $time = date("Y-m-d H:i:s");
+        $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
             ->where([
                 ['ch_id', '=', $id],
                 ['time', '<', $time]
@@ -16,15 +20,15 @@ class ProgrammList {
             ->orderBy('time', 'desc')
             ->get();
 
-            $epg = $epg->reverse();
-            $epg->pop();
-            return $epg;
-        }
+        $epg = $epg->reverse();
+        $epg->pop();
+        return $epg;
+    }
 
-        public static function getCurrentProgramm($id) {
-
-            $time = date("Y-m-d H:i:s");
-            $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
+    public static function getCurrentProgramm($id)
+    { //получаем текущую передачу
+        $time = date("Y-m-d H:i:s");
+        $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
             ->where([
                 ['ch_id', '=', $id],
                 ['time', '<=', $time],
@@ -33,13 +37,14 @@ class ProgrammList {
             ->orderBy('time', 'desc')
             ->first();
 
-            return $epg;
-        }
+        return $epg;
+    }
 
-        public static function getAfterProgramm($id, $limit_key) {
-            $time = date("Y-m-d H:i:s");
-            if ($limit_key == 1) {
-                $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
+    public static function getAfterProgramm($id, $limit_key)
+    { //получаем последующие передачи
+        $time = date("Y-m-d H:i:s");
+        if ($limit_key == 1) { //выводим 4 передачи для показа на главной странице
+            $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
                 ->where([
                     ['ch_id', '=', $id],
                     ['time', '>', $time]
@@ -47,102 +52,88 @@ class ProgrammList {
                 ->limit(4)
                 ->orderBy('time', 'asc')
                 ->get();
-
-            } else if ($limit_key == 0) {
-                $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
+        } else if ($limit_key == 0) { //выводим все передачи, что есть в базе - для страницы с полной программой
+            $epg = Epg::select('id', 'ch_id', 'time', 'time_to', 'name', 'descr')
                 ->where([
                     ['ch_id', '=', $id],
                     ['time', '>', $time]
                 ])
                 ->orderBy('time', 'asc')
                 ->get();
-            }
-            return $epg;
         }
+        return $epg;
+    }
 }
 
-class Helpers {
-        public static function correctDate($month, $day) {
-            //формируем массив с корректными названиями месяцев
-            $month_array = [
-                ' января',
-                ' февраля',
-                ' марта',
-                ' апреля',
-                ' мая',
-                ' июня',
-                ' июля',
-                ' августа',
-                ' сентября',
-                ' октября',
-                ' ноября',
-                ' декабря'
-            ];
+class Helpers
+{ //разные функции
+    public static function correctDate($month, $day)
+    {
+        //формируем массив с корректными названиями месяцев
+        $month_array = [
+            ' января',
+            ' февраля',
+            ' марта',
+            ' апреля',
+            ' мая',
+            ' июня',
+            ' июля',
+            ' августа',
+            ' сентября',
+            ' октября',
+            ' ноября',
+            ' декабря'
+        ];
 
-            //так как месяцы начинаются с 1, а элементы массива с 0, делаем поправку
-            $month_correct = $month - 1;
+        //так как месяцы начинаются с 1, а элементы массива с 0, делаем поправку
+        $month_correct = $month - 1;
 
-            //формируем красивый вывод месяца
-            $month_name = $month_array[$month_correct];
+        //формируем красивый вывод месяца
+        $month_name = $month_array[$month_correct];
 
-            //массив с правильными названиями дней недели
-            $day_array = [
-                ', понедельник',
-                ', вторник',
-                ', среда',
-                ', четверг',
-                ', пятница',
-                ', суббота',
-                ', воскресенье'
-            ];
+        //массив с правильными названиями дней недели
+        $day_array = [
+            ', понедельник',
+            ', вторник',
+            ', среда',
+            ', четверг',
+            ', пятница',
+            ', суббота',
+            ', воскресенье'
+        ];
 
-            //аналогичная поправка как с массивом месяцев
-            $day_correct = $day - 1;
+        //аналогичная поправка как с массивом месяцев
+        $day_correct = $day - 1;
 
-            //красиво выводим
-            $week_day = $day_array[$day_correct];
+        //красиво выводим
+        $week_day = $day_array[$day_correct];
 
-            return $month_name.$week_day;
+        return $month_name . $week_day;
+    }
+
+    public static function getLogo($channel_id)
+    {
+        $logo = Logos::where('ch_id', $channel_id)->first();
+
+        return $logo;
+    }
+
+    public static function getFavourSet($channel_id, $cookie_set) //формируем тип картинки, класс ссылки и блок для избранного
+    {
+        if (in_array($channel_id, $cookie_set)) {
+            $picture = 'favour_active';
+            $link_class = 'delete_favour';
+            $div = 'favour';
+        } else {
+            $picture = 'favour_ready';
+            $link_class = 'add_favour';
+            $div = '';
         }
 
-        public static function getLogo($channel_id) {
-            $logo = Logos::where('ch_id', $channel_id)->first();
-
-            return $logo;
-        }
-
-
-
-
-        public static function getFavourSet($channel_id, $cookie_set) //формируем тип картинки, класс ссылки и блок для избранного
-        {
-            if (in_array($channel_id, $cookie_set)) {
-               $picture = 'favour_active';
-               $link_class = 'delete_favour';
-               $div = 'favour';
-            } else {
-           $picture = 'favour_ready';
-           $link_class = 'add_favour';
-           $div = '';
-            }
-
-            return $favour_set = [
-                'picture' => $picture,
-                'link' => $link_class,
-                'div' => $div
-            ];
-
-        }
-
-        public static function checkLogo($logo)
-        {
-            if ($logo) {
-                $path = $logo->path;
-            } else {
-                $path = 'images/no_logo.png';
-            }
-
-            return $path;
-        }
+        return $favour_set = [
+            'picture' => $picture,
+            'link' => $link_class,
+            'div' => $div
+        ];
+    }
 }
-?>
